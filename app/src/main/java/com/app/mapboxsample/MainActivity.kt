02daa62d53c0cabfcf7497ae4ba72f809a3dbc55
jignesh <jigneshd.multiqos@gmail.com>
2024-06-25 12:29:44 +0530

    Initial commit

---
+++ app/src/main/java/com/app/mapboxsample/MainActivity.kt
@@ -0,0 +1,380 @@
+package com.app.mapboxsample
+
+import android.Manifest
+import android.content.res.Configuration
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.mapbox.common.location.LocationProvider
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.EdgeInsets
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createCircleAnnotationManager
+import com.mapbox.search.ApiType
+import com.mapbox.search.ResponseInfo
+import com.mapbox.search.SearchEngine
+import com.mapbox.search.SearchEngineSettings
+import com.mapbox.search.offline.OfflineResponseInfo
+import com.mapbox.search.offline.OfflineSearchEngine
+import com.mapbox.search.offline.OfflineSearchEngineSettings
+import com.mapbox.search.offline.OfflineSearchResult
+import com.mapbox.search.record.HistoryRecord
+import com.mapbox.search.result.SearchResult
+import com.mapbox.search.result.SearchSuggestion
+import com.mapbox.search.ui.adapter.engines.SearchEngineUiAdapter
+import com.mapbox.search.ui.adapter.location.setLocationObservationTimeout
+import com.mapbox.search.ui.view.CommonSearchViewConfiguration
+import com.mapbox.search.ui.view.DistanceUnitType
+import com.mapbox.search.ui.view.SearchResultsView
+import com.mapbox.search.ui.view.place.SearchPlace
+import com.mapbox.search.ui.view.place.SearchPlaceBottomSheetView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationProvider: LocationProvider
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var searchView: SearchView
+
+    private lateinit var searchResultsView: SearchResultsView
+    private lateinit var searchEngineUiAdapter: SearchEngineUiAdapter
+    private lateinit var searchPlaceView: SearchPlaceBottomSheetView
+
+    private lateinit var mapView: MapView
+    private lateinit var mapMarkersManager: MapMarkersManager
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(false) {
+        override fun handleOnBackPressed() {
+            when {
+                !searchPlaceView.isHidden() -> {
+                    mapMarkersManager.clearMarkers()
+                    searchPlaceView.hide()
+                }
+
+                mapMarkersManager.hasMarkers -> {
+                    mapMarkersManager.clearMarkers()
+                }
+
+                else -> {
+                    if (BuildConfig.DEBUG) {
+                        error("This OnBackPressedCallback should not be enabled")
+                    }
+                    Log.i("SearchApiExample", "This OnBackPressedCallback should not be enabled")
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        setLocationObservationTimeout(5_000L)
+
+        onBackPressedDispatcher.addCallback(onBackPressedCallback)
+
+        mapView = findViewById(R.id.map_view)
+        mapView.mapboxMap.also { mapboxMap ->
+            mapboxMap.loadStyle(getMapStyleUri())
+        }
+
+        mapMarkersManager = MapMarkersManager(mapView)
+        mapMarkersManager.onMarkersChangeListener = {
+            updateOnBackPressedCallbackEnabled()
+        }
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.apply {
+            title = getString(R.string.toolbar_title)
+            setSupportActionBar(this)
+        }
+
+        searchResultsView = findViewById<SearchResultsView>(R.id.search_results_view).apply {
+            initialize(
+                SearchResultsView.Configuration(CommonSearchViewConfiguration(DistanceUnitType.IMPERIAL))
+            )
+            isVisible = false
+        }
+
+        val searchEngineSettings = SearchEngineSettings()
+        locationProvider = searchEngineSettings.locationProvider
+            ?: throw IllegalStateException("No location provider found")
+
+        val searchEngine = SearchEngine.createSearchEngineWithBuiltInDataProviders(
+            apiType = ApiType.SEARCH_BOX, settings = searchEngineSettings
+        )
+
+        val offlineSearchEngine = OfflineSearchEngine.create(
+            OfflineSearchEngineSettings(locationProvider = locationProvider)
+        )
+
+        searchEngineUiAdapter = SearchEngineUiAdapter(
+            view = searchResultsView,
+            searchEngine = searchEngine,
+            offlineSearchEngine = offlineSearchEngine,
+        )
+
+        searchEngineUiAdapter.addSearchListener(object : SearchEngineUiAdapter.SearchListener {
+
+            override fun onSuggestionsShown(
+                suggestions: List<SearchSuggestion>, responseInfo: ResponseInfo
+            ) {
+                // Nothing to do
+            }
+
+            override fun onSearchResultsShown(
+                suggestion: SearchSuggestion,
+                results: List<SearchResult>,
+                responseInfo: ResponseInfo
+            ) {
+                closeSearchView()
+                mapMarkersManager.showMarkers(results.map { it.coordinate })
+            }
+
+            override fun onOfflineSearchResultsShown(
+                results: List<OfflineSearchResult>, responseInfo: OfflineResponseInfo
+            ) {
+                closeSearchView()
+                mapMarkersManager.showMarkers(results.map { it.coordinate })
+            }
+
+            override fun onSuggestionSelected(searchSuggestion: SearchSuggestion): Boolean {
+                return false
+            }
+
+            override fun onSearchResultSelected(
+                searchResult: SearchResult, responseInfo: ResponseInfo
+            ) {
+                closeSearchView()
+                searchPlaceView.open(SearchPlace.createFromSearchResult(searchResult, responseInfo))
+                mapMarkersManager.showMarker(searchResult.coordinate)
+            }
+
+            override fun onOfflineSearchResultSelected(
+                searchResult: OfflineSearchResult, responseInfo: OfflineResponseInfo
+            ) {
+                closeSearchView()
+                searchPlaceView.open(SearchPlace.createFromOfflineSearchResult(searchResult))
+                mapMarkersManager.showMarker(searchResult.coordinate)
+            }
+
+            override fun onError(e: Exception) {
+                Toast.makeText(applicationContext, "Error happened: $e", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onHistoryItemClick(historyRecord: HistoryRecord) {
+                closeSearchView()
+                searchPlaceView.open(
+                    SearchPlace.createFromIndexableRecord(
+                        historyRecord, distanceMeters = null
+                    )
+                )
+
+                locationProvider.userDistanceTo(
+                    this@MainActivity, historyRecord.coordinate
+                ) { distance ->
+                    distance?.let {
+                        searchPlaceView.updateDistance(distance)
+                    }
+                }
+
+                mapMarkersManager.showMarker(historyRecord.coordinate)
+            }
+
+            override fun onPopulateQueryClick(
+                suggestion: SearchSuggestion, responseInfo: ResponseInfo
+            ) {
+                if (::searchView.isInitialized) {
+                    searchView.setQuery(suggestion.name, true)
+                }
+            }
+
+            override fun onFeedbackItemClick(responseInfo: ResponseInfo) {
+                // Not implemented
+            }
+        })
+
+        searchPlaceView = findViewById(R.id.search_place_view)
+        searchPlaceView.initialize(CommonSearchViewConfiguration(DistanceUnitType.IMPERIAL))
+
+        searchPlaceView.addOnCloseClickListener {
+            mapMarkersManager.clearMarkers()
+            searchPlaceView.hide()
+        }
+
+        searchPlaceView.addOnNavigateClickListener { searchPlace ->
+            startActivity(geoIntent(searchPlace.coordinate))
+        }
+
+        searchPlaceView.addOnShareClickListener { searchPlace ->
+            startActivity(shareIntent(searchPlace))
+        }
+
+        searchPlaceView.addOnFeedbackClickListener { _, _ ->
+            // Not implemented
+        }
+
+        searchPlaceView.addOnBottomSheetStateChangedListener { _, _ ->
+            updateOnBackPressedCallbackEnabled()
+        }
+
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), PERMISSIONS_REQUEST_LOCATION
+            )
+        }else {
+            locationProvider.lastKnownLocation(this) { point ->
+                mapMarkersManager.showMarker(point!!)
+            }
+        }
+    }
+
+
+    private fun updateOnBackPressedCallbackEnabled() {
+        onBackPressedCallback.isEnabled =
+            !searchPlaceView.isHidden() || mapMarkersManager.hasMarkers
+    }
+
+    private fun closeSearchView() {
+        toolbar.collapseActionView()
+        searchView.setQuery("", false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        val searchActionView = menu.findItem(R.id.action_search)
+        searchActionView.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
+                searchPlaceView.hide()
+                searchResultsView.isVisible = true
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+                searchResultsView.isVisible = false
+                return true
+            }
+        })
+
+        searchView = searchActionView.actionView as SearchView
+        searchView.queryHint = getString(R.string.query_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                searchEngineUiAdapter.search(newText)
+                return false
+            }
+        })
+        return true
+    }
+
+    private fun getMapStyleUri(): String {
+        return when (val darkMode =
+            resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> Style.DARK
+            Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> Style.MAPBOX_STREETS
+
+            else -> error("Unknown mode: $darkMode")
+        }
+    }
+
+    private class MapMarkersManager(mapView: MapView) {
+
+        private val mapboxMap = mapView.mapboxMap
+        private val circleAnnotationManager =
+            mapView.annotations.createCircleAnnotationManager(null)
+        private val markers = mutableMapOf<String, Point>()
+
+        var onMarkersChangeListener: (() -> Unit)? = null
+
+        val hasMarkers: Boolean
+            get() = markers.isNotEmpty()
+
+        fun clearMarkers() {
+            markers.clear()
+            circleAnnotationManager.deleteAll()
+        }
+
+        fun showMarker(coordinate: Point) {
+            showMarkers(listOf(coordinate))
+        }
+
+        fun showMarkers(coordinates: List<Point>) {
+            clearMarkers()
+            if (coordinates.isEmpty()) {
+                onMarkersChangeListener?.invoke()
+                return
+            }
+
+            coordinates.forEach { coordinate ->
+                val circleAnnotationOptions: CircleAnnotationOptions =
+                    CircleAnnotationOptions().withPoint(coordinate).withCircleRadius(8.0)
+                        .withCircleColor("#ee4e8b").withCircleStrokeWidth(2.0)
+                        .withCircleStrokeColor("#ffffff")
+
+                val annotation = circleAnnotationManager.create(circleAnnotationOptions)
+                markers[annotation.id] = coordinate
+            }
+
+            if (coordinates.size == 1) {
+                CameraOptions.Builder().center(coordinates.first())
+                    .padding(MARKERS_INSETS_OPEN_CARD).zoom(16.0).build()
+            } else {
+                mapboxMap.cameraForCoordinates(
+                    coordinates, MARKERS_INSETS, bearing = null, pitch = null
+                )
+            }.also {
+                mapboxMap.setCamera(it)
+            }
+            onMarkersChangeListener?.invoke()
+        }
+    }
+
+    private companion object {
+
+        val MARKERS_EDGE_OFFSET = dpToPx(64).toDouble()
+        val PLACE_CARD_HEIGHT = dpToPx(300).toDouble()
+
+        val MARKERS_INSETS = EdgeInsets(
+            MARKERS_EDGE_OFFSET, MARKERS_EDGE_OFFSET, MARKERS_EDGE_OFFSET, MARKERS_EDGE_OFFSET
+        )
+
+        val MARKERS_INSETS_OPEN_CARD = EdgeInsets(
+            MARKERS_EDGE_OFFSET, MARKERS_EDGE_OFFSET, PLACE_CARD_HEIGHT, MARKERS_EDGE_OFFSET
+        )
+
+        const val PERMISSIONS_REQUEST_LOCATION = 0
+    }
+}
